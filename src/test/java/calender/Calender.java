e23fb69974d7bd116f6ec9f45a2d9a933bd47e5e
Nandan V <Nandan V@NandanV>
2025-12-26 15:18:35 +0530

    calender test script im doing

---
+++ src/test/java/calender/Calender.java
@@ -0,0 +1,16 @@
+package calender;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners
+public class Calender 
+{
+	@Test(groups = "regression")
+	public  void addEventInTheCalender() throws Exception 
+	{	
+//		TC_11
+	
+	}
+
+}
